e24c7f6500d0e12f52d4310a566bc832cdce2799
Katherine Sutarlim <katherine.rnslim.sutarlim@gmail.com>
2019-10-04 06:12:57 +1000

    Fix getBookableDays bugs

--- service/CalendarService.js
+++ service/CalendarService.js
@@ -1,5 +1,60 @@
 const {google} = require('googleapis');
 
+// Handles the logic associated with manipulating the calendar using Google API.
+// Exported functions: getBookableDays, getAvailableTimeSlots, and createBooking
+
+/**
+ * Checks whether there are time slots available on a specified day. This
+ * includes checking for weekends, operating hours, whether the time slot is in
+ * the past, and whether the time slot has been booked. Returns true if there
+ * are available time slots, returns false otherwise.
+ * @param {google.auth.OAuth2} auth The OAuth2 client to get token for.
+ * @param {integer} year The year containing the month to check for availability in
+ * @param {integer} month The month to check for availability in
+ * @param {integer} date The date to check for availability on
+ */
+async function isBookable(auth, year, month, date) {
+  // Check for invalid or missing parameters
+  if (!year) {
+    throw 'Request is missing parameter: year';
+  }
+  if (!month) {
+    throw 'Request is missing parameter: month';
+  }
+  if (!date) {
+    throw 'Request is missing parameter: day';
+  }
+  let timeMin = new Date(Date.UTC(year, month-1, date, 9, 0, 0, 0)); // 9AM
+  let timeMax = new Date(Date.UTC(year, month-1, date, 18, 0, 0, 0)); // 6PM
+  let numOfSlots = 12;
+  // Check if the specified day is weekend or is within 24 hours of current time
+  if (timeMax.getUTCDay() === 0 || timeMax.getUTCDay() === 6
+    || ((new Date()).getTime() + (1000*60*60*24) > timeMax)) {
+    return false;
+  }
+  // Remove time slots on the specified day that are wihin 24 hours of current
+  // time
+  while ((new Date()).getTime() + (1000*60*60*24) > timeMin) {
+    timeMin.setMinutes(startTime.getMinutes()+45);
+    numOfSlots--;
+  }
+  const calendar = google.calendar({version: 'v3', auth});
+  let result = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: timeMin.toISOString(),
+    timeMax: timeMax.toISOString(),
+    timeZone: 'UTC',
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  const events = result.data.items;
+  if (events.length === numOfSlots) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 /**
  * Checks each day in a speciied month and return whether there are time slots
  * available on that day. This includes checking for weekends, operating hours,
@@ -18,16 +73,15 @@ async function getBookableDays(auth, year, month) {
     throw 'Request is missing parameter: month';
   }
   let days = new Array();
-  let daysInMonth = new Date(Date.UTC(year, month, 0)).getDate();
   let promises = [];
-  for (var i = 1; i<=daysInMonth; i++) {
-    promises.push(getAvailableTimeSlots(auth, year, month, i));
+  let daysInMonth = new Date(Date.UTC(year, month, 0)).getDate();
+  for (var i = 1; i <= daysInMonth; i++) {
+    promises.push(isBookable(auth, year, month, i));
   }
-  const results = await Promise.all(promises);
-  // Push values into the  results array object to be returned.
-  results.map((timeSlots, i) => {
-    days.push({'day': i+1, 'hasTimeSlots': (timeSlots.length > 0)})
-  })
+  let results = await Promise.all(promises);
+  results.map((item,i) => {
+    days.push({'day': i+1, 'hasTimeSlots': (item)});
+  });
   return(days);
 }
 
@@ -64,6 +118,7 @@ async function getAvailableTimeSlots(auth, year, month, date) {
     calendarId: 'primary',
     timeMin: timeMin.toISOString(),
     timeMax: timeMax.toISOString(),
+    timeZone: 'UTC',
     singleEvents: true,
     orderBy: 'startTime',
   });
@@ -154,11 +209,13 @@ async function createBooking(auth, year, month, date, hour, minute) {
     requestBody: {
       end:
       {
-        dateTime: endTime.toISOString()
+        dateTime: endTime.toISOString(),
+        timeZone: 'UTC'
       },
       start:
       {
-        dateTime: startTime.toISOString()
+        dateTime: startTime.toISOString(),
+        timeZone: 'UTC'
       }
     }
   });
